08962e872466f7eabadd126935f3ca99e6b6e47a
Umama <umamaabidchohan@gmail.com>
2024-12-15 21:56:05 +0500

    BACKGROUND SERVICES COMPLETE

--- backgroundServices/index.js
+++ backgroundServices/index.js
@@ -4,6 +4,8 @@ import { dbConnection } from "./utils/db.js";
 import cron from "node-cron";
 import sendWelcomeEmail from "./EmailService/sendWelcomeEmail.js";
 import sendPendingOrderEmail from "./EmailService/sendPendingOrderEmail.js";
+import sendDeliveredOrder from "./EmailService/sendDeliveredOrder.js";
+import sendPromotionEmail from "./EmailService/sendPromotionEmail.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
@@ -12,9 +14,17 @@ const services = () => {
   cron.schedule("* * * * * *", async () => {
    sendWelcomeEmail();
    sendPendingOrderEmail();
+   sendDeliveredOrder();
+   sendPromotionEmail();
   });
 };
+
+const promotion = () => {
+    cron.schedule("30 5  * * 5", async () => {
+    });
+  };
 services();
+promotion();
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
   dbConnection();
